b18ab584d385dd7d2bf75693331512bceba6f966
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2016-12-14 23:51:21 -0500

    Add actions stuff.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,5 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   name: "World",
-  showName: false
+  showName: false,
+  numClicks: 0,
+  actions: {
+    toggleName() {
+      this.toggleProperty('showName');
+    },
+    incrementClicks() {
+      this.set('numClicks', this.get('numClicks') + 1);
+    }
+  }
 });
